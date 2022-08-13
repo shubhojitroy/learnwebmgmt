4a29897b11edb4d3c0a09abc92f2681dcecfb14a
shubhojit <royshubhojit@gmail.com>
2022-08-14 05:33:51 +1000

    updated webpack 5 new configuration and deleted bundled.js as in dev blundled.js served from memory

--- webpack.config.js
+++ webpack.config.js
@@ -13,8 +13,13 @@ module.exports = {
     filename: "bundled.js",
     path: path.resolve(__dirname, "app"),
   },
+  devServer: {
+    static: path.join(__dirname, "app"),
+    hot: true,
+    port: 3000,
+    host: "0.0.0.0",
+  },
   mode: "development",
-  watch: true,
   module: {
     rules: [
       {
